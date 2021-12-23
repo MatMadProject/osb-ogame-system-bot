80c111aaad6a4056ff34872da8f7483f93451d1b
madej <matmadproject@gmail.com>
2021-12-23 19:19:31 +0100

    Fix major bugs.

--- src/main/java/app/leaftask/ImperiumLeafTask.java
+++ src/main/java/app/leaftask/ImperiumLeafTask.java
@@ -118,14 +118,15 @@ public class ImperiumLeafTask extends LeafTask{
     public void planetInformation(List<Planet> list){
         for(Planet planet : list){
             //Klikanie podgląd
-            do{
-                Overview.click(OgameWeb.webDriver);
-                Waiter.sleep(200,300);
-                if(getAntiLooping().check()){
-                    getAntiLooping().reset();
-                    return;
-                }
-            }while(!Overview.visible(OgameWeb.webDriver)); // Jest niewidoczne
+            clickOverview();
+//            do{
+//                Overview.click(OgameWeb.webDriver);
+//                Waiter.sleep(200,300);
+//                if(getAntiLooping().check()){
+//                    getAntiLooping().reset();
+//                    return;
+//                }
+//            }while(!Overview.visible(OgameWeb.webDriver)); // Jest niewidoczne
 
             //Klikanie w właściwą planetę
             clickPlanet(planet);
